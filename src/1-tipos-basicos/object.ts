9d940ecf3391b2d7cdc00c130f1287a01ba394cb
Josan Johnata <josanjohnata@gmail.com>
2021-09-07 22:52:22 -0300

    Tipos básicos part 1 TypeScript OBJECT

---
+++ src/1-tipos-basicos/object.ts
@@ -0,0 +1,17 @@
+type pessoa = {
+  name: string;
+  lastName: string;
+};
+
+let pessoa: pessoa = {
+  name: 'Johnata',
+  lastName: 'Pontes'
+};
+
+
+// ou pode se usar direto no object
+
+let pessoa2: object = {
+  name: 'Johnata',
+  lastName: 'Pontes'
+};
