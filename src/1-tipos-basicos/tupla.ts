8899c8fdc9187ab04a1696ab06c55d5910a4bda1
Josan Johnata <josanjohnata@gmail.com>
2021-09-07 22:26:03 -0300

    Tipos básicos part 1 TypeScript TUPLA

---
+++ src/1-tipos-basicos/tupla.ts
@@ -0,0 +1,17 @@
+const pets: [string, string, string] = [
+  'Milk',
+  'Kiko',
+  'Logan'
+];
+
+const milk: [string, number] = [
+  'Milk', 1
+]
+
+const kiko: [string, number] = [
+  'Kiko', 3
+]
+
+const logan: [string, number] = [
+  'Logan', 5
+]
