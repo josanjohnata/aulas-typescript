e3d58f5c96fee370a3d095765164bba775383793
Josan Johnata <josanjohnata@gmail.com>
2021-09-12 20:00:55 -0300

    type Union

--- src/2-mais-tipos/type-alias-e-union-e-intersection.ts
+++ src/2-mais-tipos/type-alias-e-union-e-intersection.ts
@@ -10,3 +10,16 @@ const josan: User = {
   lastName: 'Johnata',
   birthDate: '17/03/1992'
 }
+
+// Union types quando existe alguma comparação de valores.
+// | (como se foss o ||)
+
+type LogLevel = 'info' | 'error' | 'debug';
+
+function logMessage(message: string, level: LogLevel) {
+  console.log(`[${level}] ${message}`);
+}
+
+logMessage('Uma mensagem info', 'info');
+logMessage('Uma mensagem info', 'error');
+logMessage('uma mensagem info', 'debug');
