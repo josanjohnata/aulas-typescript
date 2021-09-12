7862993837bd2341ffd8e187d3f8c9dc7694adfa
Josan Johnata <josanjohnata@gmail.com>
2021-09-12 19:47:35 -0300

    type never

--- src/2-mais-tipos/void-e-never.ts
+++ src/2-mais-tipos/void-e-never.ts
@@ -1,6 +1,21 @@
-// Quando uma funçao não tem um retorno explicito, por padrão ela retorna void(undefined)
+// Quando uma função não tem um retorno explicito, por padrão o compilador interpreta como void(undefined)
 function principal(): void {
   console.log('Executando');
-}
+};
 
 principal();
+
+// Laços de repetição infinitos
+// ou funções que disparam erros.
+function funcaoQueNuncaRetorna(): never {
+  while(true) {
+    // return alguma coisa;
+  }
+};
+
+// function funcaoQueNuncaRetorna(): never {
+//  throw new Error('Hello World')
+// };
+// const a = funcaoQueNuncaRetorna()
+
+funcaoQueNuncaRetorna();
