903ce3933b22abc1c4e0cdf957ab8b5b92de5a19
Josan Johnata <josanjohnata@gmail.com>
2021-09-12 20:28:17 -0300

    type Intersection

--- src/2-mais-tipos/type-alias-e-union-e-intersection.ts
+++ src/2-mais-tipos/type-alias-e-union-e-intersection.ts
@@ -23,3 +23,18 @@ function logMessage(message: string, level: LogLevel) {
 logMessage('Uma mensagem info', 'info');
 logMessage('Uma mensagem info', 'error');
 logMessage('uma mensagem info', 'debug');
+
+// Intersection types: &
+type About = {
+  bio: string;
+  interests: string[]
+}
+
+type Profile = User & About;
+const userWithProfile: Profile = {
+  name: 'Josan',
+  lastName: 'Johnata',
+  birthDate: '17/03/1992',
+  bio: 'Olá, meu nome é Johnata',
+  interests: ['gatos', 'tecnológia', 'churrasco']
+}
