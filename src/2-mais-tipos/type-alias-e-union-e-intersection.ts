91c3e8df7738d936c1c7b9e04d74f08a47c7c161
Josan Johnata <josanjohnata@gmail.com>
2021-09-12 19:54:16 -0300

    type alias

---
+++ src/2-mais-tipos/type-alias-e-union-e-intersection.ts
@@ -0,0 +1,12 @@
+type User = {
+  name: string;
+  lastName: string;
+  birthDate: string;
+  age?: number;
+}
+
+const josan: User = {
+  name: 'Josan',
+  lastName: 'Johnata',
+  birthDate: '17/03/1992'
+}
