5f1b4209260ca9fdb2cfe3fcdd5aa38071773f2b
Josan Johnata <josanjohnata@gmail.com>
2021-09-12 21:55:59 -0300

    orientaçao a objetos, metodo static

--- src/3-orientacao-a-objetos/classes-e-interfaces-e-heranca-e-modificadores-de-acesso.ts
+++ src/3-orientacao-a-objetos/classes-e-interfaces-e-heranca-e-modificadores-de-acesso.ts
@@ -1,19 +1,53 @@
 // Classes e interfaces e herança e modificadores de acesso.
 // class (também é possível unsar os metódos de função da mesma forma)
 
+// class User2 {
+//   constructor(nickname: string, old: number) {
+//     this.nickname = nickname;
+//     this.old = old;
+//   }
+// }
+
+// class Player extends User2 {
+//   constructor(nickname: string, old: number), game: string) {
+//     super(nickname, old);
+//     this.game = game;
+//   }
+// }
+
+// const jogador = new Player('josan', 29, 'Ghost of Tsushima');
+// console.log(jogador);
+
+
+// static
 class User2 {
-  constructor(nickname, old) {
-    this.nickname = nickname;
-    this.old = old;
+    nome: string;
+    idade: number;
+    constructor(nome: string, idade: number) {
+      this.nome = nome;
+      this.idade = idade;
+    }
   }
-}
+  
+  class Player extends User2 {
+    public jogo;
 
-class Player extends User2 {
-  constructor(nickname, old, game) {
-    super(nickname, old);
-    this.game = game;
+    constructor(nome: string, idade: number, jogo: string) {
+      super(nome, idade);
+      
+      this.jogo = jogo;
+    }
+
+    dizerOJogoAtual(){
+      return `Estou jogando, no momento: ${this.jogo}`;
+    }
+
+    static queHorasSao() {
+      return Date();
+    }
+
   }
-}
-
-const jogador = new Player('josan', 29, 'Ghost of Tsushima');
-console.log(jogador);
+  
+  const jogador = new Player('josan', 29, 'Ghost of Tsushima');
+  console.log(jogador.dizerOJogoAtual());
+  console.log(Player.queHorasSao());
