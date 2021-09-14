ba1f426eb738c3866d9f73355eb3f7440c79cf1e
Josan Johnata <josanjohnata@gmail.com>
2021-09-14 15:57:56 -0300

    classes e interfaces e heranca e modificadores de acesso

--- src/3-orientacao-a-objetos/classes-e-interfaces-e-heranca-e-modificadores-de-acesso.ts
+++ src/3-orientacao-a-objetos/classes-e-interfaces-e-heranca-e-modificadores-de-acesso.ts
@@ -1,5 +1,5 @@
 // Classes e interfaces e herança e modificadores de acesso.
-// class (também é possível unsar os metódos de função da mesma forma)
+// class (também é possível unsar os métodos de função da mesma forma)
 
 // class User2 {
 //   constructor(nickname: string, old: number) {
@@ -48,6 +48,59 @@ class User2 {
 
   }
   
-  const jogador = new Player('josan', 29, 'Ghost of Tsushima');
-  console.log(jogador.dizerOJogoAtual());
-  console.log(Player.queHorasSao());
+  // const jogador = new Player('josan', 29, 'Ghost of Tsushima');
+  // console.log(jogador.dizerOJogoAtual());
+  // console.log(Player.queHorasSao());
+
+
+  // private, protected, public
+  // public: acessível de forma geral, dentro e fora da classe
+  // private: é acessível apenas dentro da classe onde o campo foi criado
+  // protected: é acessível apenas dentro da classe (e subclasses) onde o campo foi criado
+
+  class Jogo {
+    public nome;
+
+    constructor(nome: string) {
+      this.nome = nome;
+    }
+
+    dizerNome() {
+      return `O nome do jogo é: ${this.nome}`;
+    }
+  }
+
+  // class JogoComDescricao extends Jogo {
+  //   private descricao;
+
+  //   constructor(nome: string, descricao: string) {
+  //       super(nome);
+
+  //       this.descricao = descricao;
+  //   }
+  // }
+  // const ghost = new JogoComDescricao('Ghost of Tsushima', 'É um jogo muito batuta cheio de samurai por aí');
+  // console.log(ghost.dizerNome());
+
+  // interfaces
+  interface IJogoComDescricao {
+    // nome: string;
+    descricao: string;
+    dizerNomeComDescricao(): string;
+  }
+
+  // implements
+  class JogoComDescricao extends Jogo implements IJogoComDescricao {
+    public descricao;
+
+    constructor(nome: string, descricao: string) {
+        super(nome);
+
+        this.descricao = descricao;
+    }
+
+    dizerNomeComDescricao() {
+      return `O nome do jogo é: ${this.nome}`;
+    }
+  }
+  
